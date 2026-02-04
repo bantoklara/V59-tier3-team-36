71f7a6b6951a7c520bc73508b729b8634c3e3914
Banto Klara <bantoklara2@gmail.com>
2026-02-04 15:52:27 +0200

    frontend changes

--- frontend/src/api/roles.js
+++ frontend/src/api/roles.js
@@ -1,5 +1,7 @@
 export const fetchRoles = async () => {
-    const response = await fetch('/api/roles');
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
+    console.log('API_BASE_URL:', API_BASE_URL);
+    const response = await fetch(`${API_BASE_URL}/api/roles`);
 
     if (!response.ok) {
         throw new Error('Failed to fetch roles');
